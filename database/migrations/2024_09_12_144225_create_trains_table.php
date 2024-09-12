3d0160fa56cade3310e3a5c811632c7362289643
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-12 17:08:36 +0200

    feat: migration with name of column

--- database/migrations/2024_09_12_144225_create_trains_table.php
+++ database/migrations/2024_09_12_144225_create_trains_table.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 150);
+            $table->string('departure_station', 150);
+            $table->string('arrival_station', 150);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 150);
+            $table->tinyInteger('number_carriages');
+            $table->tinyInteger('on_time');
+            $table->tinyInteger('cancelled');
             $table->timestamps();
+
+
         });
     }
 
